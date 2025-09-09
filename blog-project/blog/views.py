b6e7e5cd08b283b314f86f74a7d1efc92d866104
Eyoab Tessera <eyoabefrem9@gmail.com>
2025-09-09 18:19:22 +0300

    Blog Project

--- blog-project/blog/views.py
+++ blog-project/blog/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+from .tasks import send_post_notification
 
-# Create your views here.
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        post = serializer.save()
+        # Send async task
+        send_post_notification.delay(post.title)
